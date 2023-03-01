9f808cf620dc0344417f0303cb52dc01284da680
Your name hereclear <gustavkasche@gmail.com>
2023-03-01 13:21:31 +0100

    _

--- webserver.js
+++ webserver.js
@@ -9,5 +9,5 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
-app.listen('0.0.0.0', 80)
+app.listen(80)
 console.log('Server started at http://localhost:' + port)
